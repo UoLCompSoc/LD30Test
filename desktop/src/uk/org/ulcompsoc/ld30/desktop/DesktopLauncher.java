06ae081c83b3d4f03bf931cb2f4ef44fd43223c7
Ashley Davis (SgtCoDFish) <ashley_davis10419@hotmail.com>
2014-08-13 20:04:53 +0100

    Initial commit

---
+++ desktop/src/uk/org/ulcompsoc/ld30/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.org.ulcompsoc.ld30.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.org.ulcompsoc.ld30.LD30Test;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD30Test(), config);
+	}
+}
---
+++ html/src/uk/org/ulcompsoc/ld30/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package uk.org.ulcompsoc.ld30.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import uk.org.ulcompsoc.ld30.LD30Test;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new LD30Test();
+        }
+}
